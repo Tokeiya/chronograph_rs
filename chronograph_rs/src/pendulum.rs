15f39db2654ae65f0fdc4a1e1ec1630abf175233
Tokeiya <net_seed@hotmail.com>
2026-05-23 17:23:28 +0900

    Implement `Pendulum` trait for `InstantPendulum` with `std::time::Instant`

--- chronograph_rs/src/pendulum.rs
+++ chronograph_rs/src/pendulum.rs
@@ -1,9 +1,15 @@
-// use super::moment::Moment;
-// use super::time_span::TimeSpan;
-//
-// pub trait Pendulum
-// {
-// 	fn measurement(&self) -> T;
-// }
-//
-// pub struct InstantPenduram;
+use super::moment::Moment;
+
+pub trait Pendulum {
+	type M: Moment;
+	fn measurement(&self) -> Self::M;
+}
+
+pub struct InstantPenduram;
+
+impl Pendulum for InstantPenduram {
+	fn measurement(&self) -> <Self as Pendulum>::M {
+		std::time::Instant::now()
+	}
+	type M = std::time::Instant;
+}
