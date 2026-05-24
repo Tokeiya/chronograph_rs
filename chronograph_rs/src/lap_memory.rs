8b7998174fb662cac5ea8c3a9f587748680eaf39
Tokeiya <net_seed@hotmail.com>
2026-05-24 18:30:08 +0900

    Remove `Memory` trait implementation for `Vec<Elapsed<T>>` and add `len` method to `Memory` trait

--- chronograph_rs/src/lap_memory.rs
+++ chronograph_rs/src/lap_memory.rs
@@ -13,20 +13,5 @@ pub trait Memory<T: TimeSpan>: Index<usize, Output = Elapsed<T>> {
 	fn iter(&self) -> Self::Iter<'_>;
 
 	fn push(&mut self, value: Elapsed<T>);
-}
-
-impl<T: TimeSpan> Memory<T> for Vec<Elapsed<T>> {
-	type Iter<'a>
-		= std::slice::Iter<'a, Elapsed<T>>
-	where
-		Self: 'a,
-		T: 'a;
-
-	fn iter(&self) -> Self::Iter<'_> {
-		self.as_slice().iter()
-	}
-
-	fn push(&mut self, value: Elapsed<T>) {
-		self.push(value);
-	}
+	fn len(&self) -> usize;
 }
